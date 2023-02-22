00af6457a0c2eb1d11550d0fa32c7a0aa63e0b60
Thales Ruan <thalesruandamassacla@gmail.com>
2023-02-22 19:26:11 -0300

    refazendo tudo seguindo padroes mvvm

--- app/src/main/java/br/com/igorbag/githubsearch/domain/Repository.kt
+++
@@ -1,9 +0,0 @@
-package br.com.igorbag.githubsearch.domain
-
-import com.google.gson.annotations.SerializedName
-
-data class Repository(
-    val name: String,
-    @SerializedName("html_url")
-    val htmlUrl: String
-)
